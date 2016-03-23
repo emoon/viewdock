c943248c177510389dc87f1da498d4156e1e8511
Daniel Collin <daniel@collin.com>
2016-03-24 00:36:32 +0100

    Started with data structures

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,30 @@
+
+enum Direction {
+    Vertical,
+    Horizontal,
+}
+
+
+struct Container {
+    temp: i32,
+}
+
+struct Split {
+    split: Option<Box<Split>>,
+    leftTop: Container,
+    rightBottom: Container,
+    percent: f32,
+    direction: Direction,
+}
+
+struct Workspace {
+    split: Split,
+    x: i32,
+    y: i32,
+    width: i32,
+    height: i32,
+}
+
 #[cfg(test)]
 mod test {
     #[test]
