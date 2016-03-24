97c4f9ef5138806b8c387517487139d48030acfb
Daniel Collin <daniel@collin.com>
2016-03-24 13:52:52 +0100

    Started with Workspace::new added Error

--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,57 @@
+mod error;
+pub use self::error::Error;
 
-enum Direction {
+pub type Result<T> = std::result::Result<T, Error>;
+
+pub struct ViewHandle(u64);
+
+pub struct Rect {
+    pub x: f32,
+    pub y: f32,
+    pub width: f32,
+    pub height: f32,
+}
+
+pub struct View {
+    pub handle: ViewHandle,
+    pub rect: Rect
+}
+
+pub enum Direction {
     Vertical,
     Horizontal,
 }
 
-
-struct Container {
-    temp: i32,
+pub struct Container {
+    pub views: Vec<View>,
 }
 
-struct Split {
-    split: Option<Box<Split>>,
-    leftTop: Container,
-    rightBottom: Container,
-    percent: f32,
-    direction: Direction,
+pub struct Split {
+    pub split: Option<Box<Split>>,
+    pub left_top: Container,
+    pub right_bottom: Container,
+    pub percent: f32,
+    pub direction: Direction,
 }
 
-struct Workspace {
-    split: Split,
-    x: i32,
-    y: i32,
-    width: i32,
-    height: i32,
+pub struct Workspace {
+    pub split: Option<Split>,
+    pub view: Option<View>,
+    pub rect: Rect,
+}
+
+
+impl Workspace {
+    pub fn new(rect: Rect) -> Result<Workspace> {
+        if rect.y < 0.0 {
+            return Err(Error::IllegalSize("y has to be non-negative".to_owned()));
+        }
+
+        Ok(Workspace {
+            split: None,
+            view: None,
+            rect: rect })
+    }
 }
 
 #[cfg(test)]
